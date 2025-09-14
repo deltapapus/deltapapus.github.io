cc254060015b9bb6b603739f058cb85ba6155876
SharpINT Team <161966183+sharpint@users.noreply.github.com>
2025-09-14 01:53:19 -0400

    Cooldown

--- JS/webhook.js
+++ JS/webhook.js
@@ -2,6 +2,10 @@ const webhookURL = 'https://discord.com/api/webhooks/1416448432662642698/r5pkc6V
 
 /* WEBHOOK */
 document.getElementById('sendMsg').addEventListener('click', () => {
+    const sendBtn = document.getElementById('sendMsg');
+    sendBtn.disabled = true;
+    sendBtn.textContent = 'Espera 30s...';
+
     fetch(webhookURL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -11,4 +15,10 @@ document.getElementById('sendMsg').addEventListener('click', () => {
             avatar_url: 'https://cdn.discordapp.com/icons/1408971645569335420/a_ac331567216651740e961cb9efb7a2e5.gif?size=2048'
         })
     })
-});
+    .finally(() => {
+        setTimeout(() => {
+            sendBtn.disabled = false;
+            sendBtn.textContent = 'Enviar mensaje al Servidor.';    
+        }, 30000);
+    });
+}); 
