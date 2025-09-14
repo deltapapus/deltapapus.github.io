0e327f56179cdc585e5cbd86eeba26ad4b533ff0
SharpINT Team <161966183+sharpint@users.noreply.github.com>
2025-09-13 23:51:22 -0400

    Jijub

---
+++ JS/webhook.js
@@ -0,0 +1,14 @@
+const webhookURL = 'https://discord.com/api/webhooks/1416448432662642698/r5pkc6VcPbAkD4KfyjKsIEYLm4rKE1iH8v4KlWT8Q4zoWiRvxKrvUPRuhRPxMZCEmXPv';
+
+/* WEBHOOK */
+document.getElementById('sendMsg').addEventListener('click', () => {
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            content: 'zzz zerber -10/10',
+            username: 'hater de deltapapus',
+            avatar_url: 'https://cdn.discordapp.com/icons/1408971645569335420/a_ac331567216651740e961cb9efb7a2e5.gif?size=2048'
+        })
+    })
+});
